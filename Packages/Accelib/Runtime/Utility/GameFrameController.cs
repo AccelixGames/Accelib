c0b505e5704e7e1fd837703a4bd23ae2c5130d4f
splusk2006 <splusk2006@gmail.com>
2024-03-22 17:21:06 +0900

    Frame Controller

---
+++ Packages/Accelib/Runtime/Utility/GameFrameController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Accelib.Utility
+{
+    public class GameFrameController : MonoBehaviour
+    {
+        [SerializeField, Range(-1, 165)] private int targetFrameRate = 60;
+        
+        private void Awake()
+        {
+            Application.targetFrameRate = targetFrameRate;
+        }
+    }
+}
