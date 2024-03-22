adccab2cb402f5f203bd1a2114c0f33a6583f191
splusk2006 <splusk2006@gmail.com>
2024-03-22 17:36:08 +0900

    AspectScaleController

---
+++ Packages/Accelib/Runtime/Utility/AspectScaleController.cs
@@ -0,0 +1,25 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Accelib.Utility
+{
+    [RequireComponent(typeof(Camera))]
+    public class AspectScaleController : MonoBehaviour
+    {
+        [SerializeField] private RectTransform target;
+        [SerializeField] private float referenceHeight = 568;
+        
+        [SerializeField, ReadOnly] private float initialSize = 11.5f;
+        [SerializeField, ReadOnly] private float ratio;
+        [SerializeField, ReadOnly] private Camera cam;
+        
+        private void Start()
+        {
+            cam = GetComponent<Camera>();
+            initialSize = cam.orthographicSize;
+            
+            ratio = 1 / Mathf.Min(referenceHeight / target.sizeDelta.y, 1f);
+            cam.orthographicSize = initialSize * ratio;
+        }
+    }
+}
