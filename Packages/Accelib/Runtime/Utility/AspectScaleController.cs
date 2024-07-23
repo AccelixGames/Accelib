343d7093e4804768f52baa81b1d3fdea9b47e2a4
splusk2006 <splusk2006@gmail.com>
2024-07-23 16:31:30 +0900

    0.0.8

--- Packages/Accelib/Runtime/Utility/AspectScaleController.cs
+++ Packages/Accelib/Runtime/Utility/AspectScaleController.cs
@@ -1,14 +1,19 @@
-﻿using NaughtyAttributes;
+﻿using System;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Accelib.Utility
 {
     [RequireComponent(typeof(Camera))]
     public class AspectScaleController : MonoBehaviour
     {
-        [SerializeField] private RectTransform target;
-        [SerializeField] private float referenceHeight = 568;
+        [FormerlySerializedAs("target")]
+        [SerializeField] private RectTransform canvas;
+        [SerializeField, Range(0f, 2048f)] private float referenceHeight = 568;
+        [SerializeField] private bool autoUpdate = false; 
         
+        [Header("Size Debug")]
         [SerializeField, ReadOnly] private float initialSize = 11.5f;
         [SerializeField, ReadOnly] private float ratio;
         [SerializeField, ReadOnly] private Camera cam;
@@ -17,8 +22,23 @@ namespace Accelib.Utility
         {
             cam = GetComponent<Camera>();
             initialSize = cam.orthographicSize;
+            ratio = -1f;
+
+            UpdateRatio();
+        }
+
+        private void LateUpdate()
+        {
+            if (autoUpdate) 
+                UpdateRatio();
+        }
+
+        private void UpdateRatio()
+        {
+            var newRatio = 1f / Mathf.Min(referenceHeight / canvas.sizeDelta.y, 1f);
+            if (Mathf.Abs(ratio - newRatio) < 0.001f) return;
             
-            ratio = 1 / Mathf.Min(referenceHeight / target.sizeDelta.y, 1f);
+            ratio = newRatio;
             cam.orthographicSize = initialSize * ratio;
         }
     }
