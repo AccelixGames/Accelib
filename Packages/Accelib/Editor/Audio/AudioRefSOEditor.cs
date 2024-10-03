86e6c940517ae18d8df60fe5e759e467054c1f34
splusk2006 <splusk2006@gmail.com>
2024-10-03 16:03:59 +0900

    audio

---
+++ Packages/Accelib/Editor/Audio/AudioRefSOEditor.cs
@@ -0,0 +1,35 @@
+﻿using Accelib.Audio.Data;
+using Accelib.Logging;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Accelib.Editor.Audio
+{
+    [CustomEditor(typeof(AudioRefSO))]
+    [CanEditMultipleObjects]
+    public class AudioRefSOEditor : UnityEditor.Editor
+    {
+        public override bool HasPreviewGUI() => ((AudioRefSO)target)?.Clip != null;
+
+        private UnityEditor.Editor _editor;
+
+        public override void OnPreviewSettings()
+        {
+            var clip = ((AudioRefSO)target)?.Clip;
+            if (!clip) return;
+
+            _editor ??= CreateEditor(clip);
+            _editor?.OnPreviewSettings();
+        }
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background)
+        {
+            var clip = ((AudioRefSO)target)?.Clip;
+            if (!clip) return;
+            
+            _editor ??= CreateEditor(clip);
+            _editor?.OnPreviewGUI(r, background);
+        }
+    }
+}
