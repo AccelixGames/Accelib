cbc1857f06bf120d5b17c95ef6be74bdd884ea55
splusk2006 <splusk2006@gmail.com>
2024-11-09 01:38:41 +0900

    Update SkeletonAnimationExtension.cs

--- Packages/Accelib/Runtime/Spine/Extensions/SkeletonAnimationExtension.cs
+++ Packages/Accelib/Runtime/Spine/Extensions/SkeletonAnimationExtension.cs
@@ -6,10 +6,20 @@ namespace Accelib.Spine.Extensions
     {
         public static TrackEntry SetAnimationWithoutDuplication(this AnimationState animState, int trackIndex, string animation, bool loop)
         {
-            var curr = animState.GetCurrent(trackIndex); 
-            
-            if(curr?.Animation.Name != animation)
+            var curr = animState.GetCurrent(trackIndex);
+
+            if (curr?.Animation.Name != animation)
                 return animState.SetAnimation(trackIndex, animation, loop);
+
+            return curr;
+        }
+        
+        public static TrackEntry AddAnimationWithoutDuplication(this AnimationState animState, int trackIndex, string animation, bool loop, float delay)
+        {
+            var curr = animState.GetCurrent(trackIndex);
+
+            if (curr?.Animation.Name != animation)
+                return animState.AddAnimation(trackIndex, animation, loop, delay);
 
             return curr;
         }
