68bc4c0c47e41f81e6b421e175f7c376b00d6f57
splusk2006 <splusk2006@gmail.com>
2024-11-14 16:59:54 +0900

    Update VersionText.cs

--- Packages/Accelib/Runtime/Module.UI.Utility/VersionText.cs
+++ Packages/Accelib/Runtime/Module.UI.Utility/VersionText.cs
@@ -8,23 +8,28 @@ namespace Accelib.Module.UI.Utility
     [RequireComponent(typeof(TMP_Text))]
     public class VersionText : MonoBehaviour
     {
-        [SerializeField, ReadOnly] private string devStr;
         [SerializeField, ReadOnly] private string version;
+        [SerializeField, ReadOnly] private string devStr;
         
         private TMP_Text _target;
 
         private void OnEnable()
         {
-            #if DEVELOPMENT_BUILD
+            var demoStr = "";
+#if DEMO_BUILD
+            demoStr = "demo-";
+#endif
+            
+#if DEVELOPMENT_BUILD
             devStr = "dev";
-            #else
+#else
             devStr = "prod";
-            #endif
+#endif
 
             version = Application.version;
             
             _target = GetComponent<TMP_Text>();
-            _target.text = devStr + "-" + version;
+            _target.text = demoStr + version + devStr;
         }
     }
 }
