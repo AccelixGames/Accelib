621d7266a90161aa7d4457fd1b0e7b8c56310b5c
Kimin Kim <splusk2006@gmail.com>
2025-02-19 18:37:55 +0900

    Update Event resetter

---
+++ Packages/Accelib/Runtime/Extension.Atom/EventResetter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityAtoms;
+using UnityEngine;
+
+namespace Accelib.Extension.Atom
+{
+    public class EventResetter : MonoBehaviour
+    {
+        [SerializeField] private List<AtomEventBase> events;
+
+        private void OnEnable()
+        {
+            for (var i = 0; i < events.Count; i++)
+            {
+                events[i].UnregisterAll();
+            }
+        }
+    }
+}
