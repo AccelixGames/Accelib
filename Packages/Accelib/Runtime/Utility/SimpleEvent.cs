cd3b25b9a2b7d1044aa99f495051f7eeef1e047d
Kimin Kim <splusk2006@gmail.com>
2025-02-24 18:34:57 +0900

    Simple Event

---
+++ Packages/Accelib/Runtime/Utility/SimpleEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Accelib.Utility
+{
+    public class SimpleEvent : MonoBehaviour
+    {
+        public UnityEvent onAwake;
+        public UnityEvent onStart;
+        public UnityEvent onEnable;
+        
+        private void Awake() => onAwake?.Invoke();
+        private void Start() => onStart?.Invoke();
+        private void OnEnable() => onEnable?.Invoke();
+    }
+}
