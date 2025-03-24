5134f6f37a4725d8daff2454d04e1c808e75db6a
Kimin Kim <splusk2006@gmail.com>
2025-03-24 23:47:04 +0900

    punch & score

---
+++ Packages/Accelib/Runtime/Effect/SimplePunchEffect.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Accelib.Effect
+{
+    public class SimplePunchEffect : MonoBehaviour
+    {
+        [SerializeField] private Vector3 punch = Vector3.one;
+        [SerializeField] private float duration = 0.2f;
+        [SerializeField] private int vibrato = 10;
+        [SerializeField] private float elasticity = 1f;
+
+        private Tweener _tween;
+        
+        [Button(enabledMode: EButtonEnableMode.Playmode)]
+        public void DoPunch()
+        {
+            _tween?.Kill(true);
+            _tween = transform.DOPunchScale(punch, duration, vibrato, elasticity);
+        }
+    }
+}
---
+++ Packages/Accelib/Runtime/Extension.Atom/UI/SimpleScoreTMP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using DG.Tweening;
+using NaughtyAttributes;
+using TMPro;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Accelib.Extension.Atom.UI
+{
+    public class SimpleScoreTMP : MonoBehaviour
+    {
+        [Header("Core")]
+        [SerializeField] private TMP_Text tmp;
+        [SerializeField] private IntVariable variable;
+        
+        [Header("Value")]
+        [SerializeField, Range(0f, 10f)] private float duration = 0.5f;
+        [SerializeField, ReadOnly] private int currValue; 
+        [SerializeField, ReadOnly] private int targetValue;
+
+        [Header("Event")]
+        public UnityEvent onChanged;
+
+        private Tweener _tween;
+        
+        private void Start()
+        {
+            currValue = 0;
+            targetValue = 0;
+        }
+
+        private void OnEnable()
+        {
+            variable.Changed?.Register(OnChanged);
+            OnChanged(variable.Value);
+        }
+        
+        private void OnDisable() => variable.Changed?.Unregister(OnChanged);
+
+        private void OnChanged(int value)
+        {
+            if (targetValue == value) return;
+            
+            targetValue = value;
+            
+            _tween?.Kill();
+            _tween = DOTween.To(() => currValue, x => currValue = x, targetValue, duration)
+                .OnUpdate(() => tmp.text = currValue.ToString());
+            
+            onChanged?.Invoke();
+        }
+    }
+}
