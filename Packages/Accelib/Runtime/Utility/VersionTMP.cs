7fdd1cc14d4f040b843f648ae51514220f4ed9a9
Kimin Kim <splusk2006@gmail.com>
2025-06-12 14:12:10 +0900

    VersionTMP updated

---
+++ Packages/Accelib/Runtime/Utility/VersionTMP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Accelib.Utility
+{
+    public class VersionTMP : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text text;
+
+        private void OnEnable()
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            const string devStr = "d";
+#else
+            const string devStr = "p";
+#endif
+            var platform = Application.platform.ToString();
+            
+            // v0522.1(standalone64x-prod)
+            text.text = $"{devStr}{Application.version}({platform})";
+        }
+
+        private void Reset()
+        {
+            text = GetComponent<TMP_Text>();
+        }
+        
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (Application.isPlaying) return;
+            
+            text ??= GetComponent<TMP_Text>();
+            OnEnable();
+        }
+#endif
+    }
+}
