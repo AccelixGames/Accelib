9d04969e93fd7f6e81ad9e5b3a9f69bda3e2eb98
Kimin Kim <splusk2006@gmail.com>
2025-06-13 22:22:19 +0900

    updated webgltemplates

---
+++ Packages/Accelib/WebGLTemplates/AccelixWeb/src/sentry.js
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://cba71630b07f0f9fc21846a65fc431f1@o4509444614651904.ingest.us.sentry.io/4509467798667264",
+  sendDefaultPii: true,
+  sampleRate: 0.5, // Adjust the sample rate as needed
+  integrations: [
+    Sentry.contextLinesIntegration(),
+    Sentry.extraErrorDataIntegration()
+  ],
+});
