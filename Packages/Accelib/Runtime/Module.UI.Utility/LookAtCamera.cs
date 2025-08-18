8ab53bad5df5d911ffade6d9723c03bcd2474250
Kimin Kim <splusk2006@gmail.com>
2025-08-18 10:56:36 +0900

    up

---
+++ Packages/Accelib/Runtime/Module.UI.Utility/LookAtCamera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Accelib.Module.UI.Utility
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        [SerializeField] private Camera targetCamera;
+
+        private void OnEnable()
+        {
+            if(targetCamera == null)
+                targetCamera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            transform.rotation = targetCamera.transform.rotation;
+        }
+    }
+}
