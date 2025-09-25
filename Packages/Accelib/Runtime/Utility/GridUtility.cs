ccb21c2e565c8d63d5153136d39ac00633357fab
Kimin Kim <splusk2006@gmail.com>
2025-09-25 14:21:24 +0900

    Gizmo & Grid

--- Packages/Accelib/Runtime/Utility/GridUtility.cs
+++ Packages/Accelib/Runtime/Utility/GridUtility.cs
@@ -1,10 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Accelib.Utility
 {
     public static class GridUtility
     {
+        public static void GetGridTransform(Transform parent, Vector2Int capacity, float unitSize, float unitSpace
+        , Action<Vector3, Vector3, Quaternion> onGrid)
+        {
+            // 사이즈 캐싱
+            var size = new Vector3(1f, 1f, 1f) * unitSize;
+            var offsetY = size.y * 0.5f;
+            var center = parent.position;
+            var rot = parent.rotation; // 부모 회전 반영
+
+            // 전체 크기 계산 (x,z 방향)
+            var totalWidth = capacity.x * size.x + (capacity.x - 1) * unitSpace;
+            var totalDepth = capacity.y * size.z + (capacity.y - 1) * unitSpace;
+
+            // 왼쪽/앞쪽 기준 offset (아직 로컬 좌표)
+            var startOffset = new Vector3(-totalWidth * 0.5f + size.x * 0.5f, 0f, -totalDepth * 0.5f + size.z * 0.5f);
+
+            for (var x = 0; x < capacity.x; x++)
+            for (var y = 0; y < capacity.y; y++)
+            {
+                var localOffset = new Vector3(x * (size.x + unitSpace), offsetY, y * (size.z + unitSpace));
+
+                // 부모 회전에 맞게 offset 회전 적용
+                var worldPos = center + rot * (startOffset + localOffset);
+                onGrid?.Invoke(worldPos, size, rot);
+            }
+        }
+        
         public static List<Vector2Int> GetCoordNxN(Vector2 center, float cellSize, Vector2Int range)
         {
             // 2. 월드 좌표 → 그리드 좌표 변환
