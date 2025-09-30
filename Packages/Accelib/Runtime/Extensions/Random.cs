082366f4a9058e61f2c12b51db5b3d03bbe3f487
Kimin Kim <splusk2006@gmail.com>
2025-09-30 20:33:29 +0900

    Update Random.cs

--- Packages/Accelib/Runtime/Extensions/Random.cs
+++ Packages/Accelib/Runtime/Extensions/Random.cs
@@ -7,5 +7,14 @@ namespace Accelib.Extensions
         public static Vector2Int Vec2Int(int minInclusive, int maxInclusive) => new(
             UnityEngine.Random.Range(minInclusive, maxInclusive + 1),
             UnityEngine.Random.Range(minInclusive, maxInclusive + 1));
+
+        public static Vector3 InsideBounds(Bounds bounds) => new(
+            UnityEngine.Random.Range(bounds.min.x, bounds.max.x),
+            UnityEngine.Random.Range(bounds.min.y, bounds.max.y),
+            UnityEngine.Random.Range(bounds.min.z, bounds.max.z)
+        );
+
+        public static Vector3 InsideBounds(BoxCollider collider) => 
+            InsideBounds(collider.bounds);
     }
 }
