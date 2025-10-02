69998057f4c30aac054d4bdcc2168b29f58d98af
Kimin Kim <splusk2006@gmail.com>
2025-10-02 18:54:43 +0900

    Update MonoBehaviourExtension.cs

--- Packages/Accelib/Runtime/Extensions/MonoBehaviourExtension.cs
+++ Packages/Accelib/Runtime/Extensions/MonoBehaviourExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Accelib.Extensions
@@ -8,6 +10,18 @@ namespace Accelib.Extensions
         public static void FindComponents<T>(this Object monoBehaviour, ref List<T> list) where T : class
         {
             if(list == null) list = new List<T>();
+            else list.Clear();
+            
+            foreach (var o in Object.FindObjectsByType<MonoBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+            {
+                if(o.TryGetComponent<T>(out var comp))
+                    list.Add(comp);
+            }
+        }
+        
+        public static void FindComponents<T>(this Object monoBehaviour, ref HashSet<T> list) where T : class
+        {
+            if (list == null) list = new HashSet<T>();
             else list.Clear();
             
             foreach (var o in Object.FindObjectsByType<MonoBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None))
