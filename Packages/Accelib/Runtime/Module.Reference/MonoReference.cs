3e00bab4a99b5b62cb858efc9524822aaa00cabc
Kimin Kim <splusk2006@gmail.com>
2025-10-02 15:42:25 +0900

    Reference update

---
+++ Packages/Accelib/Runtime/Module.Reference/MonoReference.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Accelib.Module.Reference
+{
+    public abstract class MonoReference<T> : MonoBehaviour where T : Component
+    {
+        [Header("# 레퍼런스")]
+        [SerializeField] private SO_Reference<T> reference;
+
+        protected virtual void Awake() => reference.Register(GetComponent<T>());
+        protected virtual  void OnDestroy() => reference.UnRegister(GetComponent<T>());
+    }
+}
