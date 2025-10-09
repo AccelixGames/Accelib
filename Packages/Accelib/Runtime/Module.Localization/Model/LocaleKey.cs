c7862b55a362ae6ce45149b2d48661a5b31e6708
Kimin Kim <splusk2006@gmail.com>
2025-10-09 11:26:45 +0900

    Locale

---
+++ Packages/Accelib/Runtime/Module.Localization/Model/LocaleKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Accelib.Module.Localization.Model
+{
+    [System.Serializable]
+    public class LocaleKey
+    {
+        [SerializeField] private string key;
+
+        public string Key => key;
+        public void SetKey(string value) => key = value;
+        
+#if UNITY_EDITOR
+        [SerializeField, HideInInspector] 
+        private string[] preview;
+#endif
+    }
+}
