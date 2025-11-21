15d77dac9b6b7abcd9742df45c745cae8145118f
Kimin Kim <splusk2006@gmail.com>
2025-11-21 11:35:00 +0900

    언어변경 기능 추가

---
+++ Packages/Accelib/Runtime/Module.Localization/Helper/SimpleLocaleToggle.cs
@@ -0,0 +1,20 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Accelib.Module.Localization.Helper
+{
+    public class SimpleLocaleToggle : MonoBehaviour
+    {
+        [Tooltip("변경할 언어")]
+        [SerializeField] private SystemLanguage languageA = SystemLanguage.Korean;
+        [SerializeField] private SystemLanguage languageB = SystemLanguage.Korean;
+        
+        [Button("언어 토글하기", EButtonEnableMode.Playmode)]
+        public void ToggleLanguage()
+        {
+            LocalizationSingleton.ChangeLanguageStatic(LocalizationSingleton.Instance.CurrLang == languageA
+                ? languageB
+                : languageA);
+        }
+    }
+}
---
+++ Packages/Accelib/Runtime/Utility/CursorLockModeListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Accelib.Utility
+{
+    public class CursorLockModeListener : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent<CursorLockMode> onCursorLockModeChanged;
+        [SerializeField] private UnityEvent onCursorLockModeNone;
+        [SerializeField] private UnityEvent onCursorLockModeLocked;
+        [SerializeField] private UnityEvent onCursorLockModeConfined;
+
+        [Header("Debug")]
+        [SerializeField, ReadOnly] private CursorLockMode prevLockMode;
+
+        private void OnEnable()
+        {
+            prevLockMode = Cursor.lockState;
+        }
+
+        private void LateUpdate()
+        {
+            var currLockMode = Cursor.lockState;
+            if (prevLockMode == currLockMode) return;
+
+            prevLockMode = currLockMode;
+            onCursorLockModeChanged?.Invoke(currLockMode);
+            
+            if(currLockMode == CursorLockMode.None)
+                onCursorLockModeNone?.Invoke();
+            else if(currLockMode == CursorLockMode.Locked)
+                onCursorLockModeLocked?.Invoke();
+            else if(currLockMode == CursorLockMode.Confined)
+                onCursorLockModeConfined?.Invoke();
+        }
+    }
+}
