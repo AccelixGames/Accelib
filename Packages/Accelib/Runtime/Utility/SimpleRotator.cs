22c119e6b1e7916c8439c3cdb346a91490721196
Kimin Kim <splusk2006@gmail.com>
2025-11-22 23:37:45 +0900

    Add SimpleRotator and SimpleTimescale utility scripts
    Introduces SimpleRotator for smooth rotation control with acceleration and child rotation preservation, and SimpleTimescale for adjusting Unity's timescale via inspector. These utilities enhance runtime control for common game behaviors.

---
+++ Packages/Accelib/Runtime/Utility/SimpleRotator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Accelib.Utility
+{
+    public class SimpleRotator : MonoBehaviour
+    {
+        [SerializeField] private float spd;
+        [SerializeField] private float accel;
+        [SerializeField] private Vector3 rotateNormal;
+
+        [Header("# Option")]
+        [SerializeField] private bool preserveChildLocalRotation;
+        [SerializeField] private Transform child;
+        
+        [Header("# Curr")]
+        [SerializeField, ReadOnly] private bool isEnabled;
+        [SerializeField, ReadOnly] private float currSpd;
+
+        public void Toggle(bool enable) => isEnabled = enable;
+        
+        private void OnEnable()
+        {
+            rotateNormal = rotateNormal.normalized;
+            
+            isEnabled = false;
+            currSpd = 0f;
+        }
+
+        private void Update()
+        {
+            if (isEnabled)
+                currSpd += accel * Time.deltaTime;
+            else
+                currSpd -= accel * Time.deltaTime;
+            
+            currSpd = Mathf.Clamp(currSpd, 0f, spd);
+
+            var rot = Quaternion.identity;
+            if (preserveChildLocalRotation && child)
+                rot = child.rotation;
+            
+            transform.Rotate( rotateNormal * (currSpd * Time.deltaTime));
+            
+            if (preserveChildLocalRotation && child)
+                child.rotation = rot;
+        }
+    }
+}
---
+++ Packages/Accelib/Runtime/Utility/SimpleTimescale.cs
@@ -0,0 +1,14 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Accelib.Utility
+{
+    public class SimpleTimescale : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 5f)] private float targetTimescale = 1f;
+        [ShowNativeProperty] private float CurrTimescale => Time.timeScale;
+        
+        [Button(enabledMode: EButtonEnableMode.Playmode)]
+        public void SetTimescale() => Time.timeScale = targetTimescale;
+    }
+}
